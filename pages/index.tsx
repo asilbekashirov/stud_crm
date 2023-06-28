c6bb49059df2f01f624d7ba4c9bf5be4df70cab7
asilbekashirov <asilbekashirov03@gmail.com>
2023-06-28 16:11:14 +0300

    errors fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,12 @@
-import { RootState } from "@/redux/store/store"
 import { useRouter } from "next/router"
 import { useEffect } from "react"
-import { useSelector } from "react-redux"
 
 export default function Home() {
 
-  const {isAuth} = useSelector((state: RootState) => state.app)
   const router = useRouter()
 
   useEffect(() => {
-    router.push("/auth")
+    router.push("/login")
   }, [])
 
   return null
