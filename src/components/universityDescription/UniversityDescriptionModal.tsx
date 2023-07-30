801fbfc85d1632415d78ed4444f973eb9b9210d3
asilbekashirov <asilbekashirov03@gmail.com>
2023-07-30 23:27:44 +0300

    fix

--- src/components/universityDescription/UniversityDescriptionModal.tsx
+++ src/components/universityDescription/UniversityDescriptionModal.tsx
@@ -41,7 +41,7 @@ const UniversityDescriptionModal: FC<IProps> = (props) => {
         <motion.div
           layoutId={selectedId!}
           className={[
-            "rounded-lg bg-slate-100 flex flex-col md:w-3/5 w-11/12",
+            "rounded-lg bg-primary-700 flex flex-col md:w-3/5 w-11/12",
             styles.card,
           ].join(" ")}
         >
@@ -51,7 +51,7 @@ const UniversityDescriptionModal: FC<IProps> = (props) => {
               alt={name.en}
               className="w-full h-full object-cover rounded-t-lg block"
             />
-            <div className="w-full bg-gradient-to-b from-transparent to-black absolute top-0 left-0 h-full"></div>
+            <div className="w-full bg-gradient-to-b from-white to-black absolute top-0 left-0 h-full"></div>
             <motion.h5 className="text-2xl absolute bottom-0 left-0 md:ml-4 ml-2 text-white font-semibold mb-3">
               {name.en}
             </motion.h5>
